29a58228d9ef654818a3bb59b972581a801e8238
leandl <leandrocrispim891@gmail.com>
2023-09-19 12:38:07 -0300

    + Melhorado sistema de autenticação ao projeto da Platform.

--- platform/src/middleware.ts
+++ platform/src/middleware.ts
@@ -1,40 +1,17 @@
 import { withAuth, NextRequestWithAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
 
-export default withAuth(
-  function middleware(request: NextRequestWithAuth) {
-    if (privateRoutes.includes(request.nextUrl.pathname)) {
-      return NextResponse.json({ message: "Access denied" }, { status: 403 });
-    }
+export default withAuth(function middleware(request: NextRequestWithAuth) {}, {
+  callbacks: {
+    authorized: ({ token }) => Boolean(token),
   },
-  {
-    callbacks: {
-      authorized: ({ token, req: request }) => {
-        const isExistsToken = Boolean(token);
-        if (!isExistsToken && publicRoutes.includes(request.nextUrl.pathname)) {
-          return true;
-        }
-
-        return isExistsToken;
-      },
-    },
-  }
-);
-
-const publicRoutes = ["/api/user", "/api/user/list"];
-
-const privateRoutes = [
-  // "/api/user",
-  "/api/score",
-  "/api/song",
-  "/api/song-sets",
-  "/api/user-auth",
-];
+});
 
 export const config = {
-  matcher: privateRoutes,
+  matcher: [
+    // "/api/user",
+    "/api/score",
+    "/api/song",
+    "/api/song-sets",
+    "/api/user-auth",
+  ],
 };
-
-function isRoute(request: NextRequestWithAuth, path: string) {
-  return request.nextUrl.pathname.startsWith(path);
-}
