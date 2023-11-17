0e2cf0565018bd20d0c78486c5ceb181478e8181
leandl <leandrocrispim891@gmail.com>
2023-11-17 20:24:25 -0300

    + Adicionada a pagina not-found.

---
+++ platform/src/app/not-found.tsx
@@ -0,0 +1,14 @@
+
+
+import "./page-not-found.styles.scss"
+
+export default function PageError() {
+  return (
+    <div className="page-error">
+      <center>
+        <h1>Error 404</h1>
+        <p>Página não encontrada ¯\_(ツ)_/¯</p>
+      </center>
+    </div>
+  )
+}
